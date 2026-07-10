bea23352744cac06dad3e6871ca6c6d3e41eefea
sakthi-123gif <thalapathysakthi227@gmail.com>
2026-07-10 09:33:48 +0530

    Create 605-CanPlaceFlowers

---
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.java
@@ -0,0 +1,18 @@
+// Last updated: 7/10/2026, 9:33:48 AM
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
