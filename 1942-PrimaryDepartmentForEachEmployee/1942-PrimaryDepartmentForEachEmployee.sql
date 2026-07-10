ce6a258536f1cf52f24f8f1573ae1c7d5946fd0f
sakthi-123gif <thalapathysakthi227@gmail.com>
2026-07-10 09:31:33 +0530

    Create 1942-PrimaryDepartmentForEachEmployee

---
+++ 1942-PrimaryDepartmentForEachEmployee/1942-PrimaryDepartmentForEachEmployee.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/10/2026, 9:31:33 AM
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag='Y' OR 
+    employee_id in
+    (SELECT employee_id
+    FROM Employee
+    Group by employee_id
+    having count(employee_id)=1)
