74d3c96f46d7b53c62c9b6a89fff3f80b4b330fe
sakthi-123gif <thalapathysakthi227@gmail.com>
2026-07-10 09:32:37 +0530

    Create 1338-QueriesQualityAndPercentage

---
+++ 1338-QueriesQualityAndPercentage/1338-QueriesQualityAndPercentage.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/10/2026, 9:32:37 AM
+select
+query_name,
+round(avg(cast(rating as decimal) / position), 2) as quality,
+round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2) as poor_query_percentage
+from
+queries
+group by
+query_name;
