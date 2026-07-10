b094ac83c1f8c52e3f421e95d72ee9257aa07961
sakthi-123gif <thalapathysakthi227@gmail.com>
2026-07-10 09:32:49 +0530

    Create 1253-SortTheMatrixDiagonally

---
+++ 1253-SortTheMatrixDiagonally/1253-SortTheMatrixDiagonally.java
@@ -0,0 +1,17 @@
+// Last updated: 7/10/2026, 9:32:49 AM
+class Solution {
+      public int[][] diagonalSort(int[][] A) {
+        int m = A.length, n = A[0].length;
+        HashMap<Integer, PriorityQueue<Integer>> d = new HashMap<>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                d.putIfAbsent(i - j, new PriorityQueue<>());
+                d.get(i - j).add(A[i][j]);
+            }
+        }
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                A[i][j] = d.get(i - j).poll();
+        return A;
+    }
+}
