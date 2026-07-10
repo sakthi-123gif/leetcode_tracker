425c1e8135983984848a3e1c08e3fe9694df7dff
sakthi-123gif <thalapathysakthi227@gmail.com>
2026-07-10 09:33:15 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,12 @@
+// Last updated: 7/10/2026, 9:33:15 AM
+class Solution {
+    public boolean divisorGame(int n) {
+            if(n%2==0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+  
+    }
