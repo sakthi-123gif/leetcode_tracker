adea77e655d553b1079a5f73ff912da8178f88ef
sakthi-123gif <thalapathysakthi227@gmail.com>
2026-07-10 09:33:22 +0530

    Create 1001-NRepeatedElementInSize2nArray

---
+++ 1001-NRepeatedElementInSize2nArray/1001-NRepeatedElementInSize2nArray.java
@@ -0,0 +1,13 @@
+// Last updated: 7/10/2026, 9:33:22 AM
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n=nums.length-1;
+        if(nums[0]==nums[n]) 
+        return nums[0];
+        for(int i=0;i<n;i++){
+            if(nums[i]==nums[i+1]|| nums[i]==nums[i+2])
+            return nums[i];
+        }
+        return 0;
+    }
+}
