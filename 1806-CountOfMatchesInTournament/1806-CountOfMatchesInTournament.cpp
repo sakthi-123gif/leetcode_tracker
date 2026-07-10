ea9f070e773ccce17517eaf7651d02f69a53ea1b
sakthi-123gif <thalapathysakthi227@gmail.com>
2026-07-10 09:31:44 +0530

    Create 1806-CountOfMatchesInTournament

---
+++ 1806-CountOfMatchesInTournament/1806-CountOfMatchesInTournament.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/10/2026, 9:31:44 AM
+class Solution {
+public:
+    int numberOfMatches(int n) {
+//         int match=0;int sum=0;
+        
+//         while(n!=0){
+//             if(n%2==0){
+//             sum+=n/2;
+//             n=n/10;
+//         }
+//         else{
+
+//         }
+//     }
+//     return sum;
+// }
+return n-1;
+}
+};
