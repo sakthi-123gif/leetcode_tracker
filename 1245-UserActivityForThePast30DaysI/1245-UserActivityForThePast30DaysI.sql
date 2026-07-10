870c540b2c55ec3c68f0f62cf19d3c49ce7bf639
sakthi-123gif <thalapathysakthi227@gmail.com>
2026-07-10 09:32:52 +0530

    Create 1245-UserActivityForThePast30DaysI

---
+++ 1245-UserActivityForThePast30DaysI/1245-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,5 @@
+-- Last updated: 7/10/2026, 9:32:52 AM
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE (activity_date > "2019-06-27" AND activity_date <= "2019-07-27")
+GROUP BY activity_date;
