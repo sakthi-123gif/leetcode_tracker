991c75f02711159ea25f0d464561ac8968cd6b6d
sakthi-123gif <thalapathysakthi227@gmail.com>
2026-07-10 09:31:19 +0530

    Create 2087-ConfirmationRate

---
+++ 2087-ConfirmationRate/2087-ConfirmationRate.sql
@@ -0,0 +1,5 @@
+-- Last updated: 7/10/2026, 9:31:19 AM
+# Write your MySQL query statement below
+select s.user_id, round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+from Signups as s left join Confirmations as c on s.user_id= c.user_id group by user_id;
+
