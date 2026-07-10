ddf70f56444f50671b21eb3c5b9f90568f328242
sakthi-123gif <thalapathysakthi227@gmail.com>
2026-07-10 09:33:04 +0530

    Create 1155-ProductSalesAnalysisIii

---
+++ 1155-ProductSalesAnalysisIii/1155-ProductSalesAnalysisIii.sql
@@ -0,0 +1,7 @@
+-- Last updated: 7/10/2026, 9:33:03 AM
+select product_id, year as first_year, quantity, price 
+from sales where (product_id, year) in (
+    select product_id, min(year)
+    from sales
+    group by product_id
+)
